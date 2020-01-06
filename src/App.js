035cb1134e713a4f161992765082b09dfbaecb3b
Eduardo Gomes <elgsantos@gmail.com>
2020-01-06 00:35:24 -0300

    incluindo actions, reducers e store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from "react-redux";
 import { ProductsList } from './views/ProductsList';
 import ProductComponent from './views/Product';
+import { Store } from './store';
 
 function App() {
   return (
-    <div className="main-app">
-      <h1>Amazing Store</h1>
-      <BrowserRouter>
-        <Route exact path="/" component={ProductsList} />
-        <Route path="/product/:id" component={ProductComponent} />
-      </BrowserRouter>
-    </div>
+    <Provider store={Store}>
+      <div className="main-app">
+        <h1>Amazing Store</h1>
+        <BrowserRouter>
+          <Route exact path="/" component={ProductsList} />
+          <Route path="/product/:id" component={ProductComponent} />
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
